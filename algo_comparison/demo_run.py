13f7ea8f33c1d955e009bc10943b5a70cfbbb21e
EngineerDanny <agyapongdaniel7777@gmail.com>
2023-09-15 10:39:48 -0700

    update algo_comp code

--- algo_comparison/demo_run.py
+++ algo_comparison/demo_run.py
@@ -7,11 +7,12 @@ from datetime import date
 from sklearn.linear_model import LassoCV
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
+
 sys.path.append(os.path.abspath("/projects/genomic-ml/da2343/ml_project_1/shared"))
 from model_header import *
 from constants import *
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 np.set_printoptions(threshold=np.inf)
 
 params_df = pd.read_csv("params.csv")
@@ -28,24 +29,22 @@ data_set_name = param_dict["Dataset"]
 n_sub_samples = param_dict["# of Total Samples"]
 index_of_pred_col = param_dict["Index of Prediction Col"]
 
-
-# Name some string contants
-out_dir = "/scratch/da2343/cs685fall22/data"
-out_file = out_dir + f'/my_algos_{str(date.today())}_results.csv'
-
 dataset_path = dataset_dict[data_set_name]
 n_splits = 3
 # Import the csv file of the dataset
 dataset_pd = pd.read_csv(dataset_path, header=0)
 
 
-# Create a list of alphas for the LASSOCV to cross-validate against
-threshold_param_dict = [{'threshold': [threshold]}
-                        for threshold in np.concatenate((np.linspace(0, 0.4, 5), np.linspace(0.41, 0.6, 21), np.arange(0.7, 1.01, 0.1)))]
+threshold_param_dict = [
+    {"threshold": [threshold]}
+    for threshold in np.concatenate(
+        (np.linspace(0, 0.4, 5), np.linspace(0.41, 0.6, 21), np.arange(0.7, 1.01, 0.1))
+    )
+]
 learner_dict = {
     "Featureless": Featureless(),
-    'Spearman':  SpearmanRankRegressor(),
-    'Pearson':  MyPearsonRegressor(),
+    "Spearman": SpearmanRankRegressor(),
+    "Pearson": MyPearsonRegressor(),
     "LASSO": LassoCV(random_state=1),
     "GGM": GaussianGraphicalModel(),
 }
@@ -63,9 +62,11 @@ shuffled_arr = np.split(shuffled_df_updated, n_sections)
 for ss_index, sub_section in enumerate(shuffled_arr):
     # drop only one column per every iteration to form the input matrix
     # make the column you removed the output
-    output_vec = sub_section.iloc[:, index_of_pred_col].to_numpy().ravel()  
-    input_mat = sub_section.drop(sub_section.columns[index_of_pred_col], axis=1).to_numpy()  
-    
+    output_vec = sub_section.iloc[:, index_of_pred_col].to_numpy().ravel()
+    input_mat = sub_section.drop(
+        sub_section.columns[index_of_pred_col], axis=1
+    ).to_numpy()
+
     k_fold = KFold(n_splits=n_splits, shuffle=True, random_state=1)
     for fold_id, indices in enumerate(k_fold.split(input_mat)):
         index_dict = dict(zip(["train", "test"], indices))
@@ -73,7 +74,7 @@ for ss_index, sub_section in enumerate(shuffled_arr):
         for set_name, index_vec in index_dict.items():
             set_data_dict[set_name] = {
                 "X": input_mat[index_vec],
-                "y": output_vec[index_vec]
+                "y": output_vec[index_vec],
             }
         # Loop through the learners
         # Fit the learner to the training data
@@ -86,25 +87,26 @@ for ss_index, sub_section in enumerate(shuffled_arr):
             mse = mean_squared_error(actual_y, pred_y)
             # r2_coef = r2_score(actual_y, pred_y)
 
-            test_err_list.append(pd.DataFrame({
-                "Mean Squared Error": mse,
-                # "Root Mean Squared Error": np.sqrt(mse),
-                # "R Squared": pearsonr(actual_y, pred_y)[0] ** 2,
-                # "R2 Score": r2_coef,
-                "FoldID": fold_id,
-                "# of Total Samples": n_sub_samples,
-                "Index of Subsample": ss_index,
-                "Dataset": data_set_name,
-                "Index of Predicted Column": index_of_pred_col,
-                "Algorithm": learner_name,
-            }, index=[0]))
-
+            test_err_list.append(
+                pd.DataFrame(
+                    {
+                        "Mean Squared Error": mse,
+                        # "Root Mean Squared Error": np.sqrt(mse),
+                        # "R Squared": pearsonr(actual_y, pred_y)[0] ** 2,
+                        # "R2 Score": r2_coef,
+                        "FoldID": fold_id,
+                        "# of Total Samples": n_sub_samples,
+                        "Index of Subsample": ss_index,
+                        "Dataset": data_set_name,
+                        "Index of Predicted Column": index_of_pred_col,
+                        "Algorithm": learner_name,
+                    },
+                    index=[0],
+                )
+            )
 main_test_err_df = pd.concat(test_err_list)
-
 
 # Save dataframe as a csv to output directory
 out_file = f"results/{param_row}.csv"
-os.system("mkdir -p " + out_dir)
-# main_pred_actual_df.to_csv(out_file, encoding='utf-8', index=False)
-main_test_err_df.to_csv(out_file, encoding='utf-8', index=False)
+main_test_err_df.to_csv(out_file, encoding="utf-8", index=False)
 print("Done!!")
--- algo_comparison/params.py
+++ algo_comparison/params.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import shutil
 import sys
+
 sys.path.append(os.path.abspath("/projects/genomic-ml/da2343/ml_project_1/shared"))
 from constants import *
 
@@ -18,23 +19,24 @@ for dataset in dataset_list:
     # 9 for ioral, 10 for crohns, 30 for amgut1
     # n_fold = 9 if dataset_name == "ioral" else 10
     n_fold = 7
-    n_row_fold = int(n_row/n_fold)
-    n_row_fold_vec = np.ones(n_fold, dtype=int)*n_row_fold
-    n_row_fold_vec[:n_row % n_fold] += 1
+    n_row_fold = int(n_row / n_fold)
+    n_row_fold_vec = np.ones(n_fold, dtype=int) * n_row_fold
+    n_row_fold_vec[: n_row % n_fold] += 1
     n_row_fold_cumsum = np.cumsum(n_row_fold_vec)
 
     n_col_list = np.arange(n_col)
 
     params_dict = {
-        'Dataset': [dataset_name],
-        '# of Total Samples': n_row_fold_cumsum,
-        'Index of Prediction Col': n_col_list,
+        "Dataset": [dataset_name],
+        "# of Total Samples": n_row_fold_cumsum,
+        "Index of Prediction Col": n_col_list,
     }
 
-    params_df = pd.MultiIndex.from_product(
-        params_dict.values(),
-        names=params_dict.keys()
-    ).to_frame().reset_index(drop=True)
+    params_df = (
+        pd.MultiIndex.from_product(params_dict.values(), names=params_dict.keys())
+        .to_frame()
+        .reset_index(drop=True)
+    )
 
     params_df_list.append(params_df)
 
@@ -46,7 +48,7 @@ date_time = datetime.now().strftime("%Y-%m-%d_%H:%M")
 job_name = f"algo_comparison_{date_time}"
 job_dir = "/scratch/da2343/" + job_name
 results_dir = os.path.join(job_dir, "results")
-os.system("mkdir -p "+results_dir)
+os.system("mkdir -p " + results_dir)
 params_concat_df.to_csv(os.path.join(job_dir, "params.csv"), index=False)
 
 run_one_contents = f"""#!/bin/bash
@@ -69,7 +71,7 @@ run_one_py = os.path.join(job_dir, "run_one.py")
 shutil.copyfile(run_orig_py, run_one_py)
 orig_dir = os.path.dirname(run_orig_py)
 orig_results = os.path.join(orig_dir, "results")
-os.system("mkdir -p "+orig_results)
+os.system("mkdir -p " + orig_results)
 orig_csv = os.path.join(orig_dir, "params.csv")
 params_concat_df.to_csv(orig_csv, index=False)
 

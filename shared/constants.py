577010697079b0374572a43d3bab97a3731aa5a2
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-02-05 10:46:17 -0700

    Refactor dataset_list in params.py

--- shared/constants.py
+++ shared/constants.py
@@ -9,7 +9,7 @@ def get_df_from_path(path):
 
 root_data_dir = "/projects/genomic-ml/da2343/ml_project_1/data"
 
-
+'''
 dataset_list = [
     
     {"dataset_name": "Dec22_all_power",
@@ -47,9 +47,8 @@ dataset_dict = {
     "Dec22_same_melan_high_log": f"{root_data_dir}/necromass/Dec22_bacteria_fungi_conservative_r_same_melan_high_log_transformed.csv",
     "Dec22_same_melan_low_log": f"{root_data_dir}/necromass/Dec22_bacteria_fungi_conservative_r_same_melan_low_log_transformed.csv"
 }
-
-
 '''
+
 dataset_list = [
     
     ## Necromass datasets with species level data
@@ -79,12 +78,12 @@ dataset_list = [
     # {"dataset_name": "necromass_fungi_conservative",
     #     "dataframe": get_df_from_path(f"{root_data_dir}/necromass/fungi_conservative_power_transformed.csv")},
     
-    {"dataset_name": "necromass_bacteria_fungi_conservative",
-        "dataframe": get_df_from_path(f"{root_data_dir}/necromass/bacteria_fungi_conservative_power_transformed.csv")},
+    # {"dataset_name": "necromass_bacteria_fungi_conservative",
+    #     "dataframe": get_df_from_path(f"{root_data_dir}/necromass/bacteria_fungi_conservative_power_transformed.csv")},
     
 
-    # {"dataset_name": "amgut1",
-    #     "dataframe": get_df_from_path(f"{root_data_dir}/amgut1_data_power_transformed.csv")},
+    {"dataset_name": "amgut1",
+        "dataframe": get_df_from_path(f"{root_data_dir}/amgut1_data_power_transformed.csv")},
     
     # {"dataset_name": "amgut1_standard_scaled",
     #     "dataframe": get_df_from_path(f"{root_data_dir}/amgut1_data_standard_scaled.csv")},
@@ -92,20 +91,20 @@ dataset_list = [
     # {"dataset_name": "amgut1_log1_standard_scaled",
     #     "dataframe": get_df_from_path(f"{root_data_dir}/amgut1_data_log1_standard_scaled_transformed.csv")},
 
-    # {"dataset_name": "crohns",
-    #     "dataframe": get_df_from_path(f"{root_data_dir}/crohns_data_power_transformed.csv")},
+    {"dataset_name": "crohns",
+        "dataframe": get_df_from_path(f"{root_data_dir}/crohns_data_power_transformed.csv")},
 
-    # {"dataset_name": "ioral",
-    #  "dataframe": get_df_from_path(f"{root_data_dir}/ioral_data_power_transformed.csv")},
+    {"dataset_name": "ioral",
+     "dataframe": get_df_from_path(f"{root_data_dir}/ioral_data_power_transformed.csv")},
     
-    # {"dataset_name": "amgut2",
-    #     "dataframe": get_df_from_path(f"{root_data_dir}/amgut2_data_power_transformed.csv")},
+    {"dataset_name": "amgut2",
+        "dataframe": get_df_from_path(f"{root_data_dir}/amgut2_data_power_transformed.csv")},
 
-    # {"dataset_name": "hmp2prot",
-    #  "dataframe": get_df_from_path(f"{root_data_dir}/hmp2prot_data_power_transformed.csv")},
+    {"dataset_name": "hmp2prot",
+     "dataframe": get_df_from_path(f"{root_data_dir}/hmp2prot_data_power_transformed.csv")},
 
-    # {"dataset_name": "hmp216S",
-    #  "dataframe": get_df_from_path(f"{root_data_dir}/hmp216S_data_power_transformed.csv")},
+    {"dataset_name": "hmp216S",
+     "dataframe": get_df_from_path(f"{root_data_dir}/hmp216S_data_power_transformed.csv")},
 
     # {"dataset_name": "baxter_crc",
     #     "dataframe": get_df_from_path(f"{root_data_dir}/baxter_crc_data_power_transformed.csv")},
@@ -145,4 +144,3 @@ dataset_dict = {
     'mixmpln': f'{root_data_dir}/mixmpln_real_data_power_transformed.csv',
     'soilrep': f'{root_data_dir}/soilrep_data_power_transformed.csv',
 }
-'''

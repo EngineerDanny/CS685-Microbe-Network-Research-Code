1f7f259b84de16a4ad60052aec33c02cd6d3b1ed
EngineerDanny <agyapongdaniel7777@gmail.com>
2024-02-05 12:05:31 -0700

    Update time variable and file paths in analyze.py

--- reproduce/analyze.py
+++ reproduce/analyze.py
@@ -2,26 +2,27 @@
 import pandas as pd
 from glob import glob
 
-date_time = "2024-01-24_20:40"
-date_time = "2024-01-30_13:25"
-date_time = "2024-01-30_15:14"
-date_time = "2024-01-30_15:31" # classifier
-date_time = "2024-01-31_11:30" # classifier_reg
-date_time = "2024-02-01_09:06" # classifier
-date_time = "2024-02-01_09:27" # classifier
-date_time = "2024-02-02_11:33" # classifier
-date_time = "2024-02-04_19:53" # classifier_reg
-date_time = "2024-02-04_22:36" # classifier_reg
+time = "2024-01-24_20:40"
+time = "2024-01-30_13:25"
+time = "2024-01-30_15:14"
+time = "2024-01-30_15:31" # classifier
+time = "2024-01-31_11:30" # classifier_reg
+time = "2024-02-01_09:06" # classifier
+time = "2024-02-01_09:27" # classifier
+time = "2024-02-02_11:33" # classifier
+time = "2024-02-04_19:53" # classifier_reg
+time = "2024-02-04_22:36" # classifier_reg
 
-date_time = "2024-02-05_10:50" # reg
+time = "2024-02-05_10:50" # reg
+time = "2024-02-05_12:00" # reg
 
 
 out_df_list = []
-for out_csv in glob(f"/scratch/da2343/algo_comparison_{date_time}/results/*.csv"):
+for out_csv in glob(f"/scratch/da2343/algo_comparison_{time}/results/*.csv"):
     out_df_list.append(pd.read_csv(out_csv))
 error_df = pd.concat(out_df_list)
 
 root_results_dir = "/projects/genomic-ml/da2343/ml_project_1/reproduce/results"
-error_df.to_csv(f"{root_results_dir}/{date_time}_results.csv", index=False)
+error_df.to_csv(f"{root_results_dir}/{time}_results.csv", index=False)
 
 print('done')
